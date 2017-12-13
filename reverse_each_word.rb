39ddbc17965a6d15632b790bb0fb1d27837c8372
Tabitha Sin <tabitha.sin@gmail.com>
2017-12-13 00:20:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence)
-  string_array = sentence.split
-  string_array.each do |word|
+def reverse_each_word(sentence1)
+  array = sentence.split
+  array.each do |word|
     word.reverse!
   end
   array.join(" ")
